8af6bbe4c1e8b93be503ffaab20512e43e04bcc4
olavbm <olavmarkussen@hotmail.no>
2024-08-27 15:06:50 +0200

    feat: add regularization (dropout , l2)

--- run.py
+++ run.py
@@ -28,8 +28,10 @@ class NeuralNetwork(nn.Module):
         self.linear_relu_stack = nn.Sequential(
             nn.Linear(28*28, 512),
             nn.ReLU(),
+            nn.Dropout(p=0.2),
             nn.Linear(512, 512),
             nn.ReLU(),
+            nn.Dropout(p=0.2),
             nn.Linear(512, 10),
         )
 
@@ -42,6 +44,7 @@ model = NeuralNetwork()
 learning_rate = 1e-3
 batch_size = 64
 epochs = 5
+
 # Initialize the loss function
 loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
@@ -87,7 +90,7 @@ def test_loop(dataloader, model, loss_fn):
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, weight_decay=1e-5)
 
 epochs = 10
 for t in range(epochs):
@@ -95,3 +98,5 @@ for t in range(epochs):
     train_loop(train_dataloader, model, loss_fn, optimizer)
     test_loop(test_dataloader, model, loss_fn)
 print("Done!")
+
+
